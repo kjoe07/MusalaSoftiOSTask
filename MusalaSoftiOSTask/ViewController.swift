c66452d8657cf164c6a989599a5a1dbda41afade
Yoel Jimenez del Valle <yoel07@gmail.com>
2021-11-22 13:03:28 -0500

    Initial Commit

---
+++ MusalaSoftiOSTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusalaSoftiOSTask
+//
+//  Created by kjoe on 11/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
